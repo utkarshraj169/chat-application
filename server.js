dabee65691bcb4c1a8032889f61b4fcf201284a2
Utkarsh Raj <rutkarsh49@gmail.com>
2026-01-08 20:31:32 +0530

    fix render port binding

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ io.on("connection", (socket) => {
 
 // ================= START =================
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
+
